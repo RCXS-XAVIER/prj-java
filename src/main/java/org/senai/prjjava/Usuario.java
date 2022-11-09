71e63e25f57273cddae164f114abfc1a1599e192
Raquel <rakelcristina95@gmail.com>
2022-11-09 20:55:25 -0300

    commit

---
+++ src/main/java/org/senai/prjjava/Usuario.java
@@ -0,0 +1,43 @@
+package org.senai.prjjava;
+
+import org.springframework.stereotype.Indexed;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String nome;
+
+  private String email;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
