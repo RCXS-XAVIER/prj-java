25b676e7034d6792e186ee6041b01ff47192c700
Raquel <rakelcristina95@gmail.com>
2022-11-10 14:00:28 -0300

    commit 03

--- src/main/java/org/senai/prjjava/PrjJavaApplication.java
+++ src/main/java/org/senai/prjjava/PrjJavaApplication.java
@@ -13,8 +13,10 @@ public class PrjJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrjJavaApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+		return String.format("Hello %s!", name);
 	}
+
 }
